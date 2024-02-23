5e998c6bb34c4cc26e5a4c6a7a64e89d266ca23c
Breno Daros <brenosouzadaros@gmail.com>
2024-02-23 16:49:40 -0300

    initial commit

---
+++ src/main/java/br/com/f1rst/gestaodefuncionarios/GestaoDeFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.f1rst.gestaodefuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeFuncionariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/f1rst/gestaodefuncionarios/GestaoDeFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.f1rst.gestaodefuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
