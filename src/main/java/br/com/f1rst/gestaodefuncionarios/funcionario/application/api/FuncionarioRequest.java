12ef4ca112cc7ffb6e090ed7b1ba41daf2b34116
Breno Daros <brenosouzadaros@gmail.com>
2024-02-26 17:32:44 -0300

    implementa request e response

--- src/main/java/br/com/f1rst/gestaodefuncionarios/funcionario/application/api/FuncionarioRequest.java
+++ src/main/java/br/com/f1rst/gestaodefuncionarios/funcionario/application/api/FuncionarioRequest.java
@@ -1,4 +1,15 @@
 package br.com.f1rst.gestaodefuncionarios.funcionario.application.api;
 
+import br.com.f1rst.gestaodefuncionarios.funcionario.domain.EnderecoFuncionario;
+import lombok.Value;
+
+import java.math.BigDecimal;
+
+@Value
 public class FuncionarioRequest {
+    private String nomeCompleto;
+    private String designacao;
+    private BigDecimal salario;
+    private String telefone;
+    private EnderecoFuncionario endereco;
 }
--- src/main/java/br/com/f1rst/gestaodefuncionarios/funcionario/application/api/FuncionarioResponse.java
+++ src/main/java/br/com/f1rst/gestaodefuncionarios/funcionario/application/api/FuncionarioResponse.java
@@ -1,5 +1,19 @@
 package br.com.f1rst.gestaodefuncionarios.funcionario.application.api;
 
-public class FuncionarioResponse {
+import br.com.f1rst.gestaodefuncionarios.funcionario.domain.EnderecoFuncionario;
+import lombok.Builder;
+import lombok.Value;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Value
+@Builder
+public class FuncionarioResponse {
+    private UUID idFuncionario;
+    private String nomeCompleto;
+    private String designacao;
+    private BigDecimal salario;
+    private String telefone;
+    private EnderecoFuncionario endereco;
 }
